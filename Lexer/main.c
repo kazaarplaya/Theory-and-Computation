44b7ac1420787480a2985a5bfe9b1a98a8f8118f
kazaarplaya <164322774+kazaarplaya@users.noreply.github.com>
2026-05-10 19:15:15 +0930

    created a python test script for stress testing

--- Lexer/main.c
+++ Lexer/main.c
@@ -41,22 +41,44 @@ int main(int argc, char *argv[]) {
     printf("File successfuly opened!\n");
     
     // output file
-    FILE *outputfptr;
-    outputfptr = fopen("output.txt", "w");
+    FILE *outputfptr = fopen("output.txt", "w");
+    if (outputfptr == NULL) {
+        fprintf(stderr, "Could not open output file\n");
+        fclose(fptr);
+        return EXIT_FAILURE;
+    }
 
-    Lexer lexer = initialise_lexer(create_string_buffer(fptr));
+    char *source = create_string_buffer(fptr);
+    if (source == NULL) {
+        fclose(fptr);
+        return EXIT_FAILURE;
+    }
+
+    Lexer lexer = initialise_lexer(source);
     Token curr;
+
     do {
         curr = tokenize(&lexer);
-        fprintf(outputfptr, "%-12s %-20s line=%-4d column=%-4d\n",
-            token_type_to_string(curr.type),
-            curr.lexeme,
-            curr.line,
-            curr.column);     
+
+        if(curr.type == TOKEN_ERROR){
+            fprintf(stderr, "ERROR at line %d, column %d: invalid token '%s'\n",
+                curr.line,
+                curr.column,
+                curr.lexeme);
+        } else {
+            fprintf(outputfptr, "%-12s %-20s line=%-4d column=%-4d\n",
+                token_type_to_string(curr.type),
+                curr.lexeme,
+                curr.line,
+                curr.column);   
+        }
+
     } while (curr.type != TOKEN_EOF);
 
     // free memory and pointers
     fclose(fptr);
     fclose(outputfptr);
+    free(source);
+    
     return EXIT_SUCCESS;
 }
